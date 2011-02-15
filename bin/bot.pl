9adf488a4e09cb07349e055a247fd40808686f1f
Jason May <jason.a.may@gmail.com>
2011-02-15 12:53:17 -0500

    use engine class

--- bin/bot.pl
+++ bin/bot.pl
@@ -1,68 +1,11 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use IM::Engine;
-use AE;
-use AnyEvent::Socket;
-use AnyEvent::Handle;
+use App::Termcast::Server::IRC;
 use Cwd;
-use YAML qw(LoadFile);
 
-die "Arg (unix socket path) required" unless @ARGV;
+die "Arg (socket path) required" if !@ARGV;
 
+my $socket = Cwd::abs_path($ARGV[0]);
 
-my $host = 'http://jarsonmar.org:5000/';
-#my $config = LoadFile('etc/config.yml');
-
-my $engine = IM::Engine->new(
-    interface => {
-        protocol => 'IRC',
-        credentials => {
-            server   => "verne.freenode.net",
-            port     => 6667,
-            channels => ["##netmonster"],
-            nick     => "TCbot",
-        },
-    },
-);
-
-my $h;
-my $socket = tcp_connect 'unix/', Cwd::abs_path($ARGV[0]), sub {
-    warn "connected";
-    my $fh = shift;
-    $h = AnyEvent::Handle->new(
-        fh => $fh,
-        on_error => sub {
-            my ($h, $fatal, $error) = @_;
-            if ($fatal) {
-                $h->destroy;
-                warn $error;
-                AE::cv->send;
-            }
-        },
-        on_read => sub {
-            my $h = shift;
-            $h->push_read(
-                json => sub {
-                    my ($h, $data) = @_;
-                    require JSON; warn JSON->new->pretty->encode($data);
-                    if ($data->{notice}) {
-                        if ($data->{notice} eq 'connect') {
-                            my $channel = IM::Engine::Outgoing::IRC::Channel->new(
-                                channel => '##netmonster',
-                                message => sprintf(
-                                    q[%s started termcasting on %s],
-                                        $data->{connection}{user},
-                                        $host,
-                                    ),
-                            );
-                            $engine->send_message($channel);
-                        }
-                    }
-                },
-            );
-        },
-    );
-};
-
-$engine->run;
+my $engine = App::Termcast::Server::IRC->new(socket => $socket);
